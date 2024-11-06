9adc60d03f208f83ff10d1fe829ce4a254c37ef6
Karthik Praveen <KarthikPraveen@epkgroup.in>
2024-11-06 15:34:18 +0530

    updated addeent

--- src/pages/AddEvent/AddEvent.jsx
+++ src/pages/AddEvent/AddEvent.jsx
@@ -8,6 +8,7 @@ import Swal from 'sweetalert2';
 import { useRef } from 'react';
 
 export default function AddEvent() {
+    
 
     // ------------------------------------------------------------------------------------------------
 
